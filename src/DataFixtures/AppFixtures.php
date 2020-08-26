4db11d77276c3fe155c5034a7bc0c8ffe0937f7a
medjalil <med.abdeljalil@gmail.com>
2020-08-26 18:16:02 +0100

    update2020/8/26

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Actualite;
+use App\Entity\Product;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Faker\Factory;
 
-class AppFixtures extends Fixture
-{
-    public function load(ObjectManager $manager)
-    {
-        // $product = new Product();
-        // $manager->persist($product);
+class AppFixtures extends Fixture {
 
+    public function load(ObjectManager $manager) {
+        $faker = Factory::create('fr_FR');
+        for ($i = 0; $i < 8; $i++) {
+            $actualite = new Actualite();
+            $actualite->settitre($faker->text($maxNbChars = 100));
+            $actualite->setcontenu($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $actualite->setImage($faker->imageUrl($width = 640, $height = 480, 'cats'));
+            $actualite->setCreatedAt($faker->dateTime($max = 'now'));
+            $manager->persist($actualite);
+        }
+
+        for ($j = 0; $j < 8; $j++) {
+            $product = new Product();
+            $product->setName($faker->text($maxNbChars = 100));
+            $product->setDescription($faker->realText($maxNbChars = 200, $indexSize = 2));
+            $product->setImage($faker->imageUrl($width = 640, $height = 480, 'cats'));
+            $product->setCreatedAt($faker->dateTime($max = 'now'));
+            $manager->persist($product);
+        }
         $manager->flush();
     }
+
 }
