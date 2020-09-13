22b811606f3d7fa6c43ab41a73aace252c071eb7
medjalil <med.abdeljalil@gmail.com>
2020-09-13 16:32:20 +0100

    update service

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,76 +2,70 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Routing\Annotation\Route;
-
-use Symfony\Component\HttpFoundation\Request;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\HttpFoundation\Response;
-
-use Symfony\Component\Form\Extension\Core\Type\SubmitType;
-
-use Symfony\Component\Form\Extension\Core\Type\TextType;
 use App\Entity\Contact;
-
 use App\Form\ContactType;
 use App\Repository\ArticleRepository;
-class BlogController extends AbstractController
-{
-    
-   
-    
+use App\Repository\ServiceRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController {
+
     /**
-    * @Route("/",name="home")
-    */
-    public function home(){
-        return $this->render('blog/home.html.twig');
-        
+     * @Route("/",name="home")
+     */
+    public function home(ServiceRepository $serviceRepository) {
+
+        return $this->render('blog/home.html.twig', [
+                    'services' => $serviceRepository->findBy([], ['id' => 'DESC'], 5)]);
     }
-  
-      /**
-    * @Route("/contact",name="contact")
-    */
-    public function conatct(Request $request, EntityManagerInterface $manager ,\Swift_Mailer $mailer){
+
+    /**
+     * @Route("/contact",name="contact")
+     */
+    public function conatct(Request $request, EntityManagerInterface $manager, \Swift_Mailer $mailer) {
         $form = $this->createForm(ContactType::class);
         $contact = new Contact;
         $form->handleRequest($request);
-               
-        if($form->isSubmitted() && $form->isValid()){
-            $contact=$form->getData();
-            $message =(new \Swift_Message ('Nouveau contact'))
-            // on attribue l'expediteur
-            ->setFrom('emailab@hotmail.fr')
-            //on aatribue le destinateur
-            ->setTo('dtstataouinel@gmail.com')
-            // on cree le message avec le vue twig
-            ->setBody(
-                $this->renderView('Emails/email.html.twig',compact('contact'),
-                'text/html')
-            )
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $contact = $form->getData();
+            $message = (new \Swift_Message('Nouveau contact'))
+                    // on attribue l'expediteur
+                    ->setFrom('emailab@hotmail.fr')
+                    //on aatribue le destinateur
+                    ->setTo('dtstataouinel@gmail.com')
+                    // on cree le message avec le vue twig
+                    ->setBody(
+                    $this->renderView('Emails/email.html.twig', compact('contact'),
+                            'text/html')
+                    )
             ;
             // on envoi le message
             $mailer->send($message);
-          $manager->persist($contact);
-           $manager->flush();
+            $manager->persist($contact);
+            $manager->flush();
             $this->addFlash(
-                'notice',
-                'votre message etait envoyé!');
-          
+                    'notice',
+                    'votre message etait envoyé!');
+
             return $this->redirectToRoute('home');
         }
-        
-        return $this->render('blog/contact.html.twig',[
-        'form'=>$form->createView()]);
-        
+
+        return $this->render('blog/contact.html.twig', [
+                    'form' => $form->createView()]);
     }
+
     /**
-    * @Route("/about",name="about")
-    */
-    public function about(){
+     * @Route("/about",name="about")
+     */
+    public function about() {
         return $this->render('product/about.html.twig');
-        
     }
-   
-  
-  }
+
+}
--- src/Repository/ServiceRepository.php
+++ src/Repository/ServiceRepository.php
@@ -12,10 +12,9 @@ use Doctrine\Persistence\ManagerRegistry;
  * @method Service[]    findAll()
  * @method Service[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
  */
-class ServiceRepository extends ServiceEntityRepository
-{
-    public function __construct(ManagerRegistry $registry)
-    {
+class ServiceRepository extends ServiceEntityRepository {
+
+    public function __construct(ManagerRegistry $registry) {
         parent::__construct($registry, Service::class);
     }
 
@@ -23,28 +22,28 @@ class ServiceRepository extends ServiceEntityRepository
     //  * @return Service[] Returns an array of Service objects
     //  */
     /*
-    public function findByExampleField($value)
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->orderBy('s.id', 'ASC')
-            ->setMaxResults(10)
-            ->getQuery()
-            ->getResult()
-        ;
-    }
-    */
+      public function findByExampleField($value)
+      {
+      return $this->createQueryBuilder('s')
+      ->andWhere('s.exampleField = :val')
+      ->setParameter('val', $value)
+      ->orderBy('s.id', 'ASC')
+      ->setMaxResults(10)
+      ->getQuery()
+      ->getResult()
+      ;
+      }
+     */
 
     /*
-    public function findOneBySomeField($value): ?Service
-    {
-        return $this->createQueryBuilder('s')
-            ->andWhere('s.exampleField = :val')
-            ->setParameter('val', $value)
-            ->getQuery()
-            ->getOneOrNullResult()
-        ;
-    }
-    */
+      public function findOneBySomeField($value): ?Service
+      {
+      return $this->createQueryBuilder('s')
+      ->andWhere('s.exampleField = :val')
+      ->setParameter('val', $value)
+      ->getQuery()
+      ->getOneOrNullResult()
+      ;
+      }
+     */
 }
